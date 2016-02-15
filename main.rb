7d96ecb25f01b40206c7153e9a825beab8f63d17
Dan Crews <dancrews@me.com>
2016-02-15 10:41:26 -0500

    Add Boston Open Data query

--- main.rb
+++ main.rb
@@ -8,5 +8,14 @@ require 'sinatra/reloader' if development?
 require 'pry'
 
 get '/' do
-  erb :index
+  api_result = RestClient.get 'https://data.cityofboston.gov/resource/ntv7-hwjm.json?'
+  result_hash = JSON.parse(api_result)
+  output = ''
+
+  result_hash.each do |record|
+    output << "<tr><td>#{record['total_earnings'].to_i}</td></tr>"
+  end
+
+  erb :index, :locals => {results: output}
 end
+
