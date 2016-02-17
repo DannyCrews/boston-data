e9b0ef110e39006f3a179e01421b5deaa12722d1
Dan Crews <dancrews@me.com>
2016-02-17 11:56:41 -0500

    troubleshoot dotenv

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'bundler'
 Bundler.setup
-require 'dotenv'
 require 'json'
 require 'pry'
 require 'rack-flash' #not used so far
@@ -13,50 +12,54 @@ require 'uri'
 
 set :root, File.dirname(__FILE__)
 
-Dotenv.load
-
 configure :development do
-  require 'better_errors' if development?
+  require 'better_errors'
   require 'sinatra/reloader'
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
-get '/' do
-  erb :form
+if Sinatra::Base.environment == :development
+    require 'dotenv'
+    Dotenv.load
 end
 
-post '/' do
-  cap_title = params[:title].capitalize
-  @title = params[:title]
 
-  html = "https://data.cityofboston.gov/resource/ntv7-hwjm.json?$where=title like '%#{cap_title}%'&$limit=25000&$select=avg(total_earnings)"
-  encoded_url = URI.encode html
+  get '/' do
+    erb :form
+  end
 
-  api_result = RestClient.get encoded_url
-  result_array = JSON.parse api_result
-  result = result_array.reduce Hash.new, :merge
+  post '/' do
+    cap_title = params[:title].capitalize
+    @title = params[:title]
 
-  erb :index, :locals => {results: result['avg_total_earnings'].to_f.round(2)}
-end
+    html = "https://data.cityofboston.gov/resource/ntv7-hwjm.json?$where=title like '%#{cap_title}%'&$limit=25000&$select=avg(total_earnings)"
+    encoded_url = URI.encode html
 
-get '/dataset'do
-  erb :dataset
-end
+    api_result = RestClient.get encoded_url
+    result_array = JSON.parse api_result
+    result = result_array.reduce Hash.new, :merge
 
-# Easy environment check from browser
-# get '/environment' do
-#   if development?
-#     "development"
-#   elsif production?
-#     "production"
-#   elsif test?
-#     "test"
-#   else
-#     "Who knows what environment you're in!"
-#   end
-# end
+    erb :index, :locals => {results: result['avg_total_earnings'].to_f.round(2)}
+  end
 
-not_found do
-  halt 404, 'page not found'
-end
+  get '/dataset'do
+    erb :dataset
+  end
+
+  # Easy environment check from browser
+  # get '/environment' do
+  #   if development?
+  #     "development"
+  #   elsif production?
+  #     "production"
+  #   elsif test?
+  #     "test"
+  #   else
+  #     "Who knows what environment you're in!"
+  #   end
+  # end
+
+  not_found do
+    halt 404, 'page not found'
+  end
